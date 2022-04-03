c5f8012e7ab43d1ed554fd6855dda2f0759a6ed0
sowrovsarkar63 <dev.sowrovsarkar63@gmail.com>
2022-04-04 01:44:20 +0600

    asset folder added

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -2,6 +2,16 @@ import React from 'react'
 
 export default function Hero() {
   return (
-    <div className=' min-h-screen bg-[#F8FAFB] '>Hero</div>
+    <div className='h-41 bg-[#F8FAFB] '>
+        
+        <h2>Do something </h2>
+        <br />
+        <br />
+        {/* button design */}
+        {/* <button class="py-0.5 px-4 bg-[#2488FF] text-white  rounded-full shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">
+  Save changes
+</button> */}
+
+    </div>
   )
 }
