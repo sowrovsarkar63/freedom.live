e1fd57f2c676072739e1c5ee30595ef3d32fd004
sowrovsarkar63 <dev.sowrovsarkar63@gmail.com>
2022-04-04 06:54:14 +0600

    Footer section comleted

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -4,20 +4,24 @@ export default function Footer() {
   return (
     <footer>
       {/* <section className=" mx-auto grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-3 ">\ */}
-      <section className="mt-16">
-        <div className="flex justify-evenly  ">
+      <section className="mt-16  grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-3  ">
+      
         <div className="contact">
-          <h2 className="font-medium text-xl leading-7 text-[#5C5C5C] font-poppin">
+     
+          
+          <div className="contact-info flex justify-center space-x-5 ">
+            <div>
+            <h2 className="font-medium text-xl leading-7 text-[#5C5C5C] font-poppin ">
             Contact
           </h2>
-          <p>You can contact us over </p>
-          <div className="contact-info flex justify-start space-x-5 f">
-            <div>
+            <p className="text-right">You can contact us over </p>
               <p>E-mail: </p>
               <p>Instagram: </p>
               <p>Twitter: </p>
             </div>
             <div>
+              <br /><br />
+              
               <p>office@freedom.live</p>
               <p>@freedom</p>
               <p>@freedom</p>
@@ -35,7 +39,7 @@ export default function Footer() {
             ever since the 1500s.
           </p>
         </div>
-        <div className="aboutproject w-72 mb-16">
+        <div className="aboutproject w-72 mb-16 text-center">
           <h2 className="font-medium text-xl leading-7 text-[#5C5C5C] font-poppin">
             About Project
           </h2>
@@ -46,7 +50,7 @@ export default function Footer() {
           </p>
           <button class=" bg-[#E89898] rounded-full py-1 px-6 font-poppin text-white">delete offer</button>
         </div>
-        </div>
+        
       </section>
       <p className="text-center p-5 font-poppin">
       freedom.live @ 2022
